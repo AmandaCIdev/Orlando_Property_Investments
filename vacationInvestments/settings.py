a896721030c15dd7cba8313abce2332688896f73
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-04-17 15:20:13 +0000

    Add: cryptographic signing in env and env variable in settings

--- vacationInvestments/settings.py
+++ vacationInvestments/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!km!l$u=r8spp@8c@xvazyi$km_1bf0x1qzl&gihvgd0ey+@5q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
